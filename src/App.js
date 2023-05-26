6ace808078951706c08cb425421efd27ea399ead
Lan Tian <tian.lan@landing.ai>
2023-05-26 15:42:29 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import {
+  InferenceContext,
+  InferenceResult,
+  PhotoCollector
+} from "@landingai-js/react";
+import { useState } from "react";
+
+const API_INFO = {
+  endpoint:
+    "https://predict.app.landing.ai/inference/v1/predict?endpoint_id=a39e85c1-621c-4f37-94b3-fb7bba9c2c07",
+  key: "gs5fbn09iu0fjbgz5ajnr653dwuc1mr",
+  secret: "a6vqurrbrim2pfq57g0ee0byw1jhxtpp2ef0txbz0a41hfq857ky409rmqnv3n"
+};
+
+export default function App() {
+  const [image, setImage] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <InferenceContext.Provider value={API_INFO}>
+      <div className="App">
+        <PhotoCollector setImage={setImage} />
+        <InferenceResult image={image} />
+      </div>
+    </InferenceContext.Provider>
   );
 }
-
-export default App;
